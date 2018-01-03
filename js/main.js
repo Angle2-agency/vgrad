16473b0042f9104007fa933c43b4cb08eb2cc33d
Scooter)Ok <scooterok@gmail.com>
2018-01-03 16:16:40 +0200

    slider

--- js/main.js
+++ js/main.js
@@ -11,12 +11,25 @@ var app = {
 	topSliderInit : function(){
 		app.data.topSlider = new Swiper('.swiper-container', {
 			speed : 1000,
-			parallax : true,
-			loop : true,
+			parallax : false,
+			//loop : true,
       		navigation: {      			
-        		nextEl: '.swiper-button-next',
-        		prevEl: '.swiper-button-prev',
-      		},
+        		nextEl: '.topslider-button-next',
+        		prevEl: '.topslider-button-prev',
+      		},      		
+			on: {
+				init: function () {
+					var length = this.slides.length;
+					$(this.$el).find('.topslider-pagination span.current').html('01');
+					$(this.$el).find('.topslider-pagination span.length').html(length < 10 ? '0'+length : length);
+				},
+				slideChange : function(){
+					var slide = this.activeIndex + 1;
+					$(this.$el).find('.topslider-pagination span.current').html(slide < 10 ? '0'+slide : slide);
+					console.log(this.slides);
+
+				}
+			},
     	});
 	}
 }
