db24e0c55d2f92a7a3d1350f2bcb9a2cebdbdcf4
Scooter)Ok <scooterok@gmail.com>
2018-01-10 17:46:16 +0200

    9

--- js/main.js
+++ js/main.js
@@ -67,14 +67,14 @@ var app = {
 				app.slidersInit();
 				TweenMax.to('.topslider-button-prev, .topslider-button-next', 0.7, {opacity : 1, y : 0, ease: Power2.easeIn});
 				TweenMax.to('.header__reservation', 0.7, {opacity : 1, y : 0, ease: Power2.easeIn, onComplete : function(){
+					app.eventsInit();
 					TweenMax.fromTo('.header__contacts', 0.5, {opacity : 0, x : 300}, {opacity : 1, x : 0});
 					TweenMax.to('.header__logo, .topslider-pagination', 0.7, {opacity : 1, delay : 0.5, onComplete : function(){						
 						TweenMax.to('#scroll-down', 0.7, {opacity : 1, onComplete : function(){
 							$('html').removeClass('start');
 						}});
 						app.mapInit();
-						app.datePickerInit();
-						app.eventsInit();
+						app.datePickerInit();						
 					}});
 				}});				
 			}});
@@ -159,15 +159,6 @@ var app = {
 			app.data.animDone.enjoy = true;			
 			app.animate.enjoy();
 		}
-
-		
-
-
-		
-
-		
-
-		
 	},
 	eventsInit : function(){
 		$(document).click(function(e) {
@@ -178,16 +169,16 @@ var app = {
 
 		$(document).scroll(app.scroller);
 		
-		$('.bedtype__list input').click(function(e){
+		$(document).on('click', '.bedtype__list input', function(e){
 			var type = $(this).attr('data-type');
 			if(type == 'double'){
-				$('.bedtype__list').removeClass('twin').addClass('double');
+				$(this).closest('.bedtype__list').removeClass('twin').addClass('double');
 			}else{
-				$('.bedtype__list').removeClass('double').addClass('twin');
+				$(this).closest('.bedtype__list').removeClass('double').addClass('twin');
 			}
 		});		
 
-		$('.popup__rooms_number-select i').click(function(e) {
+		$(document).on('click', '.popup__rooms_number-select i', function(e) {
 			var input = $(this).closest('.popup__rooms_number-select').find('input');
 			var val = Number(input.val());
 			if($(this).hasClass('plus')){
@@ -217,7 +208,7 @@ var app = {
 			}});
 			TweenMax.to('#popupform', 0.8, {x : '0%', ease: Power2.easeIn});
 		});
-		$('.popup__rooms_number-details .show-details').click(function(e) {			
+		$(document).on('click', '.popup__rooms_number-details .show-details', function(e) {			
 			var block = $(this).closest('.popup__rooms_number-details').find('.details-descr');
 			if(block.is(':hidden')){
 				$(this).addClass('show');
@@ -344,7 +335,7 @@ var app = {
 	        zoom: 17,
 	        scrollwheel: false,
 	        // The latitude and longitude to center the map (always required)
-	        center: new google.maps.LatLng(50.606248, 30.469001),
+	        center: $('body').width() > 720 ? new google.maps.LatLng(50.606248, 30.469001) : new google.maps.LatLng(50.606211, 30.466009),
 	        // How you would like to style the map. 
 	        // This is where you would paste any style found on Snazzy Maps.
 	        styles: [{"featureType":"all","elementType":"geometry.fill","stylers":[{"weight":"2.00"}]},{"featureType":"all","elementType":"geometry.stroke","stylers":[{"color":"#ededed"}]},{"featureType":"all","elementType":"labels.text","stylers":[{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#fff7ee"}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#fff7ee"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#eeeeee"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#7b7b7b"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#ededed"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#070707"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]}]
