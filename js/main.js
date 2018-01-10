943a302ded875b68f60afbc9c3d979977d260c43
Scooter)Ok <scooterok@gmail.com>
2018-01-10 12:21:05 +0200

    9

--- js/main.js
+++ js/main.js
@@ -298,7 +298,7 @@ var app = {
 
     	app.ourclientsSlider = new Swiper('.ourclients__slider', {
 			speed : 600,
-			slidesPerView: $('body').width() > 1040 ? 5 : 3,
+			slidesPerView: $('body').width() > 1140 ? 5 : 3,
 			spaceBetween: 0,
       		navigation: {      			
         		nextEl: '#ourclients__slider-button-next',
